912aedc6affdb8b26763fb0739edfa8351a2e4f0
Sergii Lysenko <s.lysenko@gmail.com>
2016-12-10 12:48:35 +0200

    Update Plot3.R
    Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question.

--- Plot3.R
+++ Plot3.R
@@ -8,8 +8,7 @@ SCC <- readRDS("./data/Source_Classification_Code.rds")
 Balt <- subset(NEI, fips == "24510")
 BaltPM <- aggregate(Balt[c("Emissions")]/1000, list(year = Balt$year,type = Balt$type), sum)
 
-#qplot( year, Emissions, data=BaltPM, color=type,alpha=density, geom="path",xlab = "Year",  ylab=, 
-#        main= "Total emissions from PM")
+#Create plot by type
 
 qplot(year, Emissions, data = BaltPM,  color=type, geom=c("point", "smooth"), xlab = "Year", 
       ylab = expression("Emissions of PM in Ktons"), 
